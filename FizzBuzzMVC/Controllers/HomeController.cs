0b271f6f740756d6986b868ee234495da9bc97be
Tully Endicott <tendicott48@gmail.com>
2022-06-04 16:37:38 -0500

    FizzBuzzMVC exercise complete

--- FizzBuzzMVC/Controllers/HomeController.cs
+++ FizzBuzzMVC/Controllers/HomeController.cs
@@ -38,6 +38,36 @@ namespace FizzBuzzMVC.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult FBPage(FizzBuzz fizzbuzz)
         {
+            List<string> fbitems = new();
+
+            bool fizz; //default false
+            bool buzz;
+
+            for (int i = 1; i <= 100; i++)
+            {
+                fizz = (i % fizzbuzz.FizzValue == 0);
+                buzz = (i % fizzbuzz.BuzzValue == 0);
+
+                if (fizz == true && buzz == true)
+                {
+                    fbitems.Add("FizzBuzz");
+                }
+                else if (fizz == true)
+                {
+                    fbitems.Add("Fizz");
+                }
+                else if (buzz == true)
+                {
+                    fbitems.Add("Buzz");
+                }
+                else
+                {
+                    fbitems.Add(i.ToString());
+                }
+            }
+
+            fizzbuzz.Result = fbitems;
+
             return View(fizzbuzz);
         }
 
